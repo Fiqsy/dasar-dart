f2967fa42cef06da94391a246d2ba120c5398e97
fiqihfirmansyah11 <75598919+Fiqsy@users.noreply.github.com>
2021-03-06 13:22:33 +0700

    Create variabel.dart

---
+++ variabel.dart
@@ -0,0 +1,15 @@
+void main(){
+    // membaut variabel dengan tipe data
+    String nama = "Fiqih Firmansyah";
+    int umur = 22;
+    double tinggi = 172;
+    bool isMenikah = false;
+
+    // membuat veriabel dengan kata kunci var
+    var alamat = "Medan, Indonesia";
+
+    // mencetak variabel
+    print("Nama saya $nama. Umur $umur tahun. Tinggi sekitar $tinggi cm.");
+    print("Menikah: $isMenikah");
+    print("Alamat: $alamat");
+}
